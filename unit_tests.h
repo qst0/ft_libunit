66217d57057ae727db1251ff07e5d12fc9e7d749
Mason Young <masondyoung@gmail.com>
2017-02-14 17:01:48 -0800

    unit_tests.h added

---
+++ unit_tests.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unit_tests.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myoung <myoung@student.42.us.org>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/14 16:00:33 by myoung            #+#    #+#             */
+/*   Updated: 2017/02/14 16:41:07 by myoung           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef UNIT_TESTS
+#define UNIT_TESTS
+# include <libunit.h>
+
+int		demo_launcher(void);
+int		demo_pass(void);
+int		demo_seg(void);
+int		demo_bus(void);
+int		demo_fail(void);
+
+#endif
